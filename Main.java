ddb0e6a904d53c0bccc29ed157c4dbe62eb4ba54
Mariusz <koval09603@gmail.com>
2024-04-07 18:18:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Manager("Manager Name", 50000, 0);
-        employees[1] = new Worker("Worker 1", 30000, "Position 1");
-        employees[2] = new Employee("Employee 1", 40000);
-        employees[3] = new Employee("Employee 2", 40000);
-        employees[4] = new Worker("Worker 2", 30000, "Position 2");
+        employees[0] = new Manager("Bob Johnson", 50000, 0);
+        employees[1] = new Worker("Bob Marley", 30000, "Position 1");
+        employees[2] = new Employee("Hillary Clinton", 40000);
+        employees[3] = new Employee("Joe Winter", 40000);
+        employees[4] = new Worker("John Snow", 30000, "Position 2");
 
         for (Employee employee : employees) {
             System.out.println(employee.toString());
